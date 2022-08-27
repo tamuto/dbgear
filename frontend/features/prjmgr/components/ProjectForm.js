6f22e4b57fab8e4f66770102793152a19848ab32
nana.ishimatsu <nana.ishimatsu@abi-inc.co.jp>
2022-08-27 08:03:10 +0000

    一覧表示

--- frontend/features/prjmgr/components/ProjectForm.js
+++ frontend/features/prjmgr/components/ProjectForm.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-import { Button } from '@mui/material'
+import {
+  Button, 
+  List,
+  ListItemText
+} from '@mui/material'
 
 import ProjectEntryForm from './ProjectEntryForm'
 
 const ProjectForm = () => {
   const [value, setValue] = useState(false)
+  const [projects, setProjects] = useState([])
 
   const clickHandler = () => {
     setValue(true)
@@ -15,8 +21,25 @@ const ProjectForm = () => {
     setValue(false)
   }
 
+  const _search = async () => {
+    const result = await axios.get('/data/prjinfo/')
+    console.log(result)
+    setProjects(result.data)
+  }
+
+  useEffect(() => {
+    _search()
+  }, [value])
+
   return (
     <>
+      <List>
+        {
+          projects.map((item, idx) => (
+            <ListItemText key={idx} primary={item} />
+          )) 
+        }
+      </List>
       <Button variant="contained" onClick={clickHandler}>New</Button>
       <ProjectEntryForm open={value} closeHandler={closeHandler} />
     </>
