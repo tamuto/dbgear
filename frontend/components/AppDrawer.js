d29d358c944e33f76098078360e6999f12feaa06
tamuto <mutomob@gmail.com>
2023-07-16 07:11:44 +0000

    ロゴ修正

--- frontend/components/AppDrawer.js
+++ frontend/components/AppDrawer.js
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react'
+import { Link } from 'react-router-dom'
 import {
   Drawer,
   Box,
@@ -6,12 +8,35 @@ import {
   Typography
 } from '@mui/material'
 
+import imgDBGear from '~/resources/img/dbgear.png'
+
 import TemplatesList from './TemplatesList'
 import TemplateDataList from './TemplateDataList'
 import EnvironsList from './EnvironsList'
 import useProject from '../api/useProject'
 
 const drawerWidth = 240
+
+const logo = css`
+  display: flex;
+  align-items: center;
+  background-color: #86ccce;
+  margin-top: 16px;
+  margin-left: 5px;
+  margin-right: 5px;
+  padding-left: 15px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border: solid 1px grey;
+  border-radius: 20px;
+  text-decoration: none;
+  color: rgba(0,0,0,0.87);
+  font-weight: bold;
+
+  &:visited {
+    color: rgba(0,0,0,0.87);
+  }
+`
 
 const AppDrawer = () => {
   const mainMenu = useProject(state => state.mainMenu)
@@ -31,7 +56,10 @@ const AppDrawer = () => {
         }
       }}
     >
-      <Typography component='div' sx={{ mr: 1 }}>DB Gear</Typography>
+      <Typography variant='h6' component={Link} to='/' css={logo}>
+        <img src={imgDBGear} width='32px' style={{ marginRight: '15px'}} />
+        DB Gear
+      </Typography>
       <Slide direction='down' in={mainMenu} mountOnEnter unmountOnExit>
         <Box sx={{ overflow: 'auto' }}>
           <TemplatesList />
