5e79e91b1cab7c2b2c47ec11be090960d28fe1fd
tamuto <mutomob@gmail.com>
2023-10-23 14:12:10 +0000

    #28 オプション追加

--- dbgear/main.py
+++ dbgear/main.py
@@ -12,23 +12,36 @@ def execute():
         description='database management tools'
     )
     parser.add_argument(
-        'command',
-        choices=['serve', 'check', 'apply'],
-        help='please specify the command to execute.')
-    parser.add_argument(
         '--project',
         default='database',
         help='please specify the folder for the project.')
-    parser.add_argument(
-        '--env',
-        help='target environment for the command.')
-    parser.add_argument(
-        '--deploy',
-        help='target deployment for the command.')
+
+    sub = parser.add_subparsers(dest='command', help='sub-command help')
+    sub.add_parser('serve', help='serve help')
+
+    apply_parser = sub.add_parser('apply', help='apply help')
+    apply_parser.add_argument(
+        'deploy',
+        help='target deployment.')
+    apply_parser.add_argument(
+        'env',
+        help='target environment.')
+    apply_parser.add_argument(
+        '--target',
+        help='target table.')
+    apply_parser.add_argument(
+        '--all',
+        action='store_true',
+        help='apply all tables.')
+
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
     logging.info(args)
+
+    if args.command is None:
+        parser.print_help()
+        return
 
     project = Project(args.project)
     project.read_definitions()
@@ -36,4 +49,7 @@ def execute():
     if args.command == 'serve':
         backend.run(project)
     if args.command == 'apply':
-        operations.apply(project, args.env, args.deploy)
+        if not args.all and args.target is None:
+            logging.error('please specify --target or --all')
+            return
+        operations.apply(project, args.env, args.target, args.all, args.deploy)
