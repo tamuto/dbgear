0a91134eed08e3c3d86a84a13086401dd6b155a8
tamuto <mutomob@gmail.com>
2025-08-16 12:06:16 +0000

    不要ファイルの削除

--- packages/frontend.bak/src/api/useLoading.ts
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-const useLoading = create<LodingState>((set, get) => ({
-  loading: null,
-  isLoading: () => get().loading !== null,
-  setLoading: (target) => set({ loading: target })
-}))
-
-export default useLoading
--- packages/frontend.bak/src/types/Result.ts
+++
@@ -1,8 +0,0 @@
-
-interface Result<T> {
-  status: string,
-  message: string | null,
-  data: T,
-}
-
-type Callback<T> = (result: T) => void
